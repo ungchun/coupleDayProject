2e06b05a46766e0a73b505acb8437a6b4f67028c
ungchun <leedool3003@naver.com>
2022-06-12 16:52:32 +0900

    refator: storyTabView 정리

--- coupleDayProject/Controller/MainVC/TabVC/StoryTabViewController.swift
+++ coupleDayProject/Controller/MainVC/TabVC/StoryTabViewController.swift
@@ -8,137 +8,21 @@
 import UIKit
 
 class StoryTabViewController: UIViewController {
-    lazy var tempText: UILabel = {
-       let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.text = "10일"
-        view.backgroundColor = .gray
-        return view
-    }()
-    lazy var tempText3: UILabel = {
-       let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.text = "10일"
-        view.backgroundColor = .gray
-        return view
-    }()
 
-    lazy var tempText1: UILabel = {
-       let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.text = "2022.06.06(월)"
-        view.backgroundColor = .blue
-        return view
-    }()
-    lazy var tempText4: UILabel = {
-       let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.text = "2022.06.06(월)"
-        view.backgroundColor = .blue
-        return view
-    }()
-
-    lazy var demoDivider: UILabel = {
-       let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black
-        return view
-    }()
-    lazy var demoDivider1: UILabel = {
-       let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black
-        return view
-    }()
-
-    
-    lazy var emptyView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .cyan
-        return view
-    }()
-    
-    lazy var emptyView1: UILabel = {
-        let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .orange
-        return view
-    }()
-    lazy var emptyView2: UILabel = {
-        let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .orange
-        return view
-    }()
-    
+    private var storyTabView: StoryTabView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(emptyView)
-        emptyView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        emptyView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let storyTabView = StoryTabView(frame: self.view.frame)
+        self.storyTabView = StoryTabView()
+        view.addSubview(storyTabView)
         
-        // content (가운데 들어갈 글자들)
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        let stackView1 = UIStackView(arrangedSubviews: [tempText,tempText1])
-        stackView1.axis = .vertical
-        stackView1.alignment = .fill
-//        stackView.spacing = 10
-        stackView1.translatesAutoresizingMaskIntoConstraints = false
-        
-        let stackView2 = UIStackView(arrangedSubviews: [emptyView1, stackView1, demoDivider])
-        stackView2.axis = .vertical
-        stackView2.alignment = .fill
-        stackView2.translatesAutoresizingMaskIntoConstraints = false
-        stackView2.spacing = 30
-//        emptyView1.bottomAnchor.constraint(equalTo: tempText.topAnchor, constant: -10).isActive = true
-        emptyView1.heightAnchor.constraint(equalToConstant: 5).isActive = true
-//        demoDivider.topAnchor.constraint(equalTo: tempText1.bottomAnchor, constant: 10).isActive = true
-        demoDivider.heightAnchor.constraint(equalToConstant: 5).isActive = true
-        
-        let demoStackView = UIStackView(arrangedSubviews: [tempText3,tempText4])
-        demoStackView.axis = .vertical
-        demoStackView.alignment = .fill
-//        stackView.spacing = 10
-        demoStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let stackView3 = UIStackView(arrangedSubviews: [emptyView2, demoStackView, demoDivider1])
-        stackView3.axis = .vertical
-        stackView3.alignment = .fill
-        stackView3.translatesAutoresizingMaskIntoConstraints = false
-        stackView3.spacing = 30
-//        emptyView1.bottomAnchor.constraint(equalTo: tempText.topAnchor, constant: -10).isActive = true
-        emptyView2.heightAnchor.constraint(equalToConstant: 5).isActive = true
-//        demoDivider.topAnchor.constraint(equalTo: tempText1.bottomAnchor, constant: 10).isActive = true
-        demoDivider1.heightAnchor.constraint(equalToConstant: 5).isActive = true
-        
-        let stackView = UIStackView(arrangedSubviews: [emptyView, stackView2, stackView3])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-//        stackView.spacing = 10
-        
-        
-        
-        view.addSubview(scrollView)
-        scrollView.addSubview(stackView)
-        
-
-        let constraints = [
-            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ]
-        NSLayoutConstraint.activate(constraints)
-        
+        storyTabView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            storyTabView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            storyTabView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            storyTabView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            storyTabView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+        ])
     }
 }
--- coupleDayProject/View/MainView/TabView/StoryTabView.swift
+++ coupleDayProject/View/MainView/TabView/StoryTabView.swift
@@ -5,4 +5,130 @@
 //  Created by 김성훈 on 2022/06/10.
 //
 
-import Foundation
+import UIKit
+
+class StoryTabView: UIView {
+    
+    // MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: UI
+    // ㅁ 일
+    private lazy var storyDayText: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "10일"
+        view.backgroundColor = .gray
+        return view
+    }()
+    // yyyy.MM.dd
+    private lazy var storyFormatterDayText: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "2022.06.06(월)"
+        view.backgroundColor = .blue
+        return view
+    }()
+    // D-10
+    private lazy var storyD_DayText: UILabel = {
+       let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "D-10"
+        view.backgroundColor = .gray
+        return view
+    }()
+    // 하단 divider
+    private lazy var divider: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+    // 상단 탭이랑 안겹치게 주는 뷰
+    private lazy var emptyView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .cyan
+        return view
+    }()
+    private lazy var stackViewTopPadding: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .orange
+        return view
+    }()
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    // stackView 에서 가운데 날짜 관련 택스트 view
+    private lazy var textStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [storyDayText,storyFormatterDayText])
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    private lazy var contentHorizontalStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [textStackView, storyD_DayText])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+//        stackView.spacing = 50
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    // 위 아래 패딩 + divider -> stackView
+    private lazy var contentVerticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [stackViewTopPadding, contentHorizontalStackView, divider])
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 30
+        return stackView
+    }()
+
+
+    
+    // emptyView + scrollView (stackView)
+    private lazy var EntireStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emptyView, contentVerticalStackView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    // MARK: func
+    fileprivate func setup() {
+        self.addSubview(scrollView)
+        scrollView.addSubview(EntireStackView)
+        NSLayoutConstraint.activate([
+            emptyView.topAnchor.constraint(equalTo: self.topAnchor),
+            emptyView.heightAnchor.constraint(equalToConstant: 100),
+            
+            stackViewTopPadding.heightAnchor.constraint(equalToConstant: 5),
+            
+            divider.heightAnchor.constraint(equalToConstant: 5),
+            
+            EntireStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            EntireStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            EntireStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            EntireStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            EntireStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            scrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+}
