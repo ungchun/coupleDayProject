9ff8edc4336bd205dd01744a1ba440a026cfd2a7
sunghun <leedool3003@gmail.com>
2023-06-26 00:51:29 +0900

    :hammer: [chore] BaseView, BaseVC 추가 #12

---
+++ coupleDayProject/Presentation/Common/BaseView.swift
@@ -0,0 +1,12 @@
+//
+//  BaseView.swift
+//  coupleDayProject
+//
+//  Created by Kim SungHun on 2023/06/26.
+//
+
+import UIKit
+
+class BaseView: UIView {
+	
+}
---
+++ coupleDayProject/Presentation/Common/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  coupleDayProject
+//
+//  Created by Kim SungHun on 2023/06/26.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		setLayout()
+		setupView()
+	}
+	
+	func setLayout() { }
+	
+	func setupView() { }
+}
